8ae2eb17dd15f5a4f6581afd841c76aee3f3e984
Zach Richardson <richardson.227@wright.edu>
2019-02-24 15:43:31 -0500

    Framework in place, hourly scheduled created

---
+++ app/src/main/java/group3/eyemirror/ListAdapter.java
@@ -0,0 +1,63 @@
+package group3.eyemirror;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+import java.util.ArrayList;
+
+public class ListAdapter extends RecyclerView.Adapter <ListAdapter.MyViewHolder> {
+    ArrayList events;
+    ArrayList timesList;
+    Context context;
+    public ListAdapter(Context context, ArrayList times, ArrayList events) {
+        this.context = context;
+        this.events = events;
+        this.timesList = times;
+    }
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int i) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.row_layout, parent, false);
+        MyViewHolder mv = new MyViewHolder(v);
+        return mv;
+    }
+
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, final int position) {
+        // set the data in items
+        holder.time.setText(timesList.get(position).toString());
+        holder.event.setText(events.get(position).toString());
+        // implement setOnClickListener event on item view.
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // display a toast
+                Toast.makeText(context, timesList.get(position).toString(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return timesList.size();
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        TextView time;
+        TextView event;
+        public MyViewHolder(View itemView) {
+            super(itemView);
+            // get the reference of item view's
+            time = (TextView) itemView.findViewById(R.id.timeView);
+            event = (TextView) itemView.findViewById(R.id.eventView);
+
+        }
+    }
+}
+
---
+++ app/src/main/java/group3/eyemirror/MainActivity.java
@@ -0,0 +1,124 @@
+package group3.eyemirror;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+    ArrayList times = new ArrayList();
+    ArrayList events = new ArrayList();
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        RecyclerView list = (RecyclerView) findViewById(R.id.recycler);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+
+        times.addAll(Arrays.asList("12:00 am", "1:00 am", "2:00 am", "3:00 am", "4:00 am", "5:00 am", "6:00 am", "7:00 am", "8:00 am", "9:00 am", "10:00 am", "11:00 am", "12:00 pm", "1:00 pm", "2:00 pm", "3:00 pm", "4:00 pm", "5:00 pm", "6:00 pm", "7:00 pm", "8:00 pm", "9:00 pm", "10:00 pm", "11:00 pm"));
+        for (int i = 0; i <= 24; i++){
+            events.add("");
+        }
+        events.add(1, "dsfsdfd sdfdsfdfssdf sdfdsfasdfa sdfasfaadsfdfs dsfsdfdsfdasf adsfdfsasdfadfs dsfasdfadfsdfs sdfsdfadfsa dsfdsfadfsdfs asdfdfsa");
+        events.add(1, "dsfsdfd sdfdsfdfssdf sdfdsfasdfa sdfasfaadsfdfs dsfsdfdsfdasf adsfdfsasdfadfs dsfasdfadfsdfs sdfsdfadfsa dsfdsfadfsdfs asdfdfsa");
+        events.add(9, "dsfsdfd sdfdsfdfssdf sdfdsfasdfa sdfasfaadsfdfs dsfsdfdsfdasf adsfdfsasdfadfs dsfasdfadfsdfs sdfsdfadfsa dsfdsfadfsdfs asdfdfsa");
+        events.add(10, "dsfsdfd sdfdsfdfssdf sdfdsfasdfa sdfasfaadsfdfs dsfsdfdsfdasf adsfdfsasdfadfs dsfasdfadfsdfs sdfsdfadfsa dsfdsfadfsdfs asdfdfsa");
+        LinearLayoutManager layout = new LinearLayoutManager(getApplicationContext());
+        list.setLayoutManager(layout);
+        ListAdapter l = new ListAdapter(MainActivity.this, times, events);
+        list.setAdapter(l);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_camera) {
+            // Handle the camera action
+        } else if (id == R.id.nav_gallery) {
+
+        } else if (id == R.id.nav_slideshow) {
+
+        } else if (id == R.id.nav_manage) {
+
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+}
